92430a934d39bf41d3d3f54227cb28ac9b8f714b
iaaam0 <beloved.eob@gmail.com>
2024-03-07 12:01:00 +0900

    TOP버튼 추가

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $(".top-btn").fadeIn(); 
+        }
+            else {
+                $(".top-btn").fadeOut();
+                }
+            });
+
+        $(".top-btn").click(function(event){
+            event.preventDefault();
+        $("html, body").animate({ 
+            scrollTop : 0 
+        }, 500);
+            });
+});
